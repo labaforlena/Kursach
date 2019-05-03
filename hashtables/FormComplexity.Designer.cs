4cb06c74947ed266f2faa545cb50f0d16b0a18dd
Qlorry <kostik.cu4er@gmail.com>
2019-05-03 11:24:49 -0700

    Update FormComplexity.Designer.cs

--- hashtables/FormComplexity.Designer.cs
+++ hashtables/FormComplexity.Designer.cs
@@ -33,23 +33,24 @@
             this.btnEasy = new System.Windows.Forms.Button();
             this.btnHard = new System.Windows.Forms.Button();
             this.btnLeave = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Silver;
-            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(120, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.Size = new System.Drawing.Size(191, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Choose Complexity";
             // 
             // btnEasy
             // 
-            this.btnEasy.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEasy.Location = new System.Drawing.Point(170, 84);
+            this.btnEasy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEasy.Location = new System.Drawing.Point(12, 111);
             this.btnEasy.Name = "btnEasy";
             this.btnEasy.Size = new System.Drawing.Size(119, 33);
             this.btnEasy.TabIndex = 1;
@@ -59,8 +60,8 @@
             // 
             // btnHard
             // 
-            this.btnHard.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHard.Location = new System.Drawing.Point(170, 132);
+            this.btnHard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHard.Location = new System.Drawing.Point(300, 111);
             this.btnHard.Name = "btnHard";
             this.btnHard.Size = new System.Drawing.Size(119, 32);
             this.btnHard.TabIndex = 2;
@@ -70,8 +71,8 @@
             // 
             // btnLeave
             // 
-            this.btnLeave.Font = new System.Drawing.Font("Lucida Handwriting", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLeave.Location = new System.Drawing.Point(194, 179);
+            this.btnLeave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeave.Location = new System.Drawing.Point(176, 200);
             this.btnLeave.Name = "btnLeave";
             this.btnLeave.Size = new System.Drawing.Size(75, 23);
             this.btnLeave.TabIndex = 3;
@@ -79,12 +80,24 @@
             this.btnLeave.UseVisualStyleBackColor = true;
             this.btnLeave.Click += new System.EventHandler(this.btnLeave_Click);
             // 
+            // btnMedium
+            // 
+            this.btnMedium.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedium.Location = new System.Drawing.Point(156, 110);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(119, 32);
+            this.btnMedium.TabIndex = 4;
+            this.btnMedium.Text = "medium";
+            this.btnMedium.UseVisualStyleBackColor = true;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
             // FormComplexity
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(461, 211);
+            this.ClientSize = new System.Drawing.Size(431, 252);
+            this.Controls.Add(this.btnMedium);
             this.Controls.Add(this.btnLeave);
             this.Controls.Add(this.btnHard);
             this.Controls.Add(this.btnEasy);
@@ -102,5 +115,6 @@
         private System.Windows.Forms.Button btnEasy;
         private System.Windows.Forms.Button btnHard;
         private System.Windows.Forms.Button btnLeave;
+        private System.Windows.Forms.Button btnMedium;
     }
 }
