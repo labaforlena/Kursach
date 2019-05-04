10c9017d3aa1e29533d670a092b3a6677e727e5a
Qlorry <kostik.cu4er@gmail.com>
2019-05-03 23:27:56 -0700

    Create Connection.cs

---
+++ hashtables/Connection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace hashtables
+{
+    class Connection
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string datasource = "104.248.19.220";
+
+            string database = "test";
+            string username = "Test";
+            string password = "1234";
+
+
+            string connString = "server=" + datasource + ";Port=3306;Database=" + database + ";Uid=" + username + ";Pwd=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            return conn;
+        }
+
+        public static void updDB()
+        {
+
+        }
+
+        public static void getDB()
+        {
+
+        }
+    }
+}
